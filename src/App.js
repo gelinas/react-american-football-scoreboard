ed08a279c044604962e7ba9f4fa82860104748dc
gelinas <jlgelinas@gmail.com>
2019-09-10 15:50:15 -0500

    MVP

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ function App() {
       <section className="scoreboard">
         <div className="topRow">
           <div className="home">
-            <h2 className="home__name">Lions</h2>
+            <h2 className="home__name">New Mexico State</h2>
 
             {/* TODO STEP 3 - We need to change the hardcoded values in these divs to accept dynamic values from our state. */}
 
@@ -21,7 +21,7 @@ function App() {
           </div>
           <div className="timer">00:03</div>
           <div className="away">
-            <h2 className="away__name">Tigers</h2>
+            <h2 className="away__name">Alabama</h2>
             <div className="away__score">{awayScore}</div>
           </div>
         </div>
